a6301ca03cea84188f771b099f478c7e279eb934
Rubén Martín Rodríguez <r.martinrodriguez@student.tudelft.nl>
2021-12-03 19:41:48 +0100

    Progressing with minimum snap

--- traj_optimization/minimum_snap_optimization.py
+++ traj_optimization/minimum_snap_optimization.py
@@ -1,5 +1,8 @@
 import numpy as np
 import math
+import cvxpy as cp
+import matplotlib.pyplot as plt
+
 
 def get_q(n_seg, n_order, ts):
     q = np.zeros((n_seg*(n_order+1), n_seg*(n_order+1)))
@@ -10,9 +13,8 @@ def get_q(n_seg, n_order, ts):
         for i in range(4, n_order+1):
             for l in range(4, n_order+1):
                 q_k[i, l] = i*(i-1)*(i-2)*(i-3)*l*(l-1)*(l-2)*(l-3)*pow(ts[k], (i+l-7))/(i+l-7)
-
         # Assign it to the corresponding position of the general Q matrix
-        q[8*k:(8*k+7), 8*k:(8*k+7)] = q_k
+        q[8*k:(8*k+8), 8*k:(8*k+8)] = q_k
     return q
 
 
@@ -27,12 +29,13 @@ def get_ab(n_seg, n_order, waypoints, ts, start_cond, end_cond):
             aeq_end[k, -(n_order + 1 - i)] = (math.factorial(i)*pow(ts[-1], (i-k)))/math.factorial(i-k)
     beq_start = start_cond
     beq_end = end_cond
+    beq_edge = np.array([beq_start, beq_end])
 
     # position constraints for waypoints
     aeq_wp = np.zeros((n_seg-1, n_all_poly))
     for j in range(n_seg-1):
         aeq_wp[j, 8*(j+1)] = 1
-    beq_wp = waypoints[1:-2]
+    beq_wp = waypoints[1:-1]
 
     # position continuity
     aeq_contp = np.zeros((n_seg - 1, n_all_poly))
@@ -55,7 +58,7 @@ def get_ab(n_seg, n_order, waypoints, ts, start_cond, end_cond):
     for j in range(n_seg - 1):
         for i in range(2, n_order + 1):
             aeq_conta[j, 8 * j + i] = math.factorial(i) * pow(ts[j], i-2) / math.factorial(i - 2)
-        aeq_conta[j, 8 * (j + 1) + 2] = -1
+        aeq_conta[j, 8 * (j + 1) + 2] = -2
     beq_conta = np.zeros((n_seg - 1,))
 
     # jerk continuity
@@ -63,11 +66,53 @@ def get_ab(n_seg, n_order, waypoints, ts, start_cond, end_cond):
     for j in range(n_seg - 1):
         for i in range(3, n_order + 1):
             aeq_contj[j, 8 * j + i] = math.factorial(i) * pow(ts[j], i-3) / math.factorial(i - 3)
-        aeq_contj[j, 8 * (j + 1) + 3] = -1
+        aeq_contj[j, 8 * (j + 1) + 3] = -6
     beq_contj = np.zeros((n_seg - 1,))
 
     aeq_cont = np.vstack((aeq_contp, aeq_contv, aeq_conta, aeq_contj))
-    beq_cont = np.vstack((beq_contp, beq_contv, beq_conta, beq_contj))
+    beq_cont = np.concatenate((beq_contp, beq_contv, beq_conta, beq_contj))
     aeq = np.vstack((aeq_start, aeq_end, aeq_wp, aeq_cont))
-    beq = np.vstack((beq_start, beq_end, beq_wp, beq_cont))
+    beq = np.concatenate((beq_edge, beq_wp, beq_cont))
     return aeq, beq
+
+
+def compute_proportional_t(waypoints, T, n_seg):
+    ts = np.zeros((n_seg,))
+    dist = np.zeros((n_seg,))
+    dist_sum, t_sum = 0, 0
+    for i in range(n_seg):
+        dist[i] = np.linalg.norm(waypoints[i+1, :] - waypoints[i, :])
+        dist_sum += dist[i]
+    for i in range(n_seg-1):
+        ts[i] = T*dist[i]/dist_sum
+        t_sum += ts[i]
+    ts[-1] = T - t_sum
+    return ts
+
+
+def minimum_snap_qp(waypoints, ts, n_seg, n_order):
+    start_cond = np.array([path[0, 0], 0, 0, 0])
+    end_cond = np.array([path[-1, 0], 0, 0, 0])
+    qs = get_q(n_seg, n_order, ts)
+    aeq, beq = get_ab(n_seg, n_order, waypoints, ts, start_cond, end_cond)
+    print(aeq.shape)
+    print(beq.shape)
+    x = cp.Variable(n_seg * (n_order + 1))
+    prob = cp.Problem(cp.Minimize(cp.quad_form(x, qs)), [aeq @ x == beq])
+    prob.solve()
+    return x.value
+
+
+path = np.array([[0, 0], [1, 4], [2, 2], [3, 6]])
+n_order = 7
+n_seg = np.size(path, axis=0) - 1
+#ts = compute_proportional_t(path, 25, n_seg)
+ts = np.full((n_seg,), 1)
+poly_coef_x = minimum_snap_qp(path[:, 0], ts, n_seg, n_order)
+poly_coef_y = minimum_snap_qp(path[:, 1], ts, n_seg, n_order)
+
+fig = plt.figure()
+plt.scatter(path[:, 0], path[:, 1])
+
+
+
